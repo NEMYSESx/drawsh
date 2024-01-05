3fc0a10a21a4225b6a8c02981bc45b274ab58deb
NEMYSESx <111594052+NEMYSESx@users.noreply.github.com>
2024-01-05 13:55:12 +0530

    completed the whiteboard controller

---
+++ src/controllers/whiteBoard.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import drawingModel from "../models/drawingModel.js";
+
+class Whiteboard {
+  userId: string;
+  path: string;
+  createdAt: Date;
+
+  constructor(props: { userId: string; path: string; createdAt: Date }) {
+    this.userId = props.userId;
+    this.path = props.path;
+    this.createdAt = props.createdAt;
+  }
+
+  static async createWhiteBoard(req: Request, res: Response): Promise<void> {
+    try {
+      const body = req.body;
+      const whiteBoard = new drawingModel({
+        userId: body.userId,
+        path: body.path,
+        createdAt: new Date(),
+      });
+
+      await whiteBoard.save();
+      res.status(201).json({ message: "Whiteboard created successfully" });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: "Failed to create whiteboard" });
+    }
+  }
+
+  static async getWhiteBoard(req: Request, res: Response): Promise<void> {
+    try {
+      const board = await drawingModel.findOne({
+        req: Request.params.whiteBoard,
+      });
+      if (!board) {
+        res.status(404).json({ error: "board not found" });
+      }
+      res.status(200).json({ message: "Whiteboard came successfully" });
+    } catch (err) {
+      console.log(err.message);
+      res.send(500).json({ error: "failed to get whiteBoard" });
+    }
+  }
+
+  static async delWhiteBoard(req: Request, res: Response): Promise<void> {
+    try {
+      const board = await drawingModel.findOneAndDelete({
+        req: Request.params.whiteBoard,
+      });
+      if (!board) {
+        res.status(404).json({ error: "board not found" });
+      } else {
+        res.status(200).json({ message: "Whiteboard deleted successfully" });
+      }
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json({ err: "failed to delete whiteboard" });
+    }
+  }
+}
